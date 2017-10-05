22cfeaa909aebed5cec05948306fc6f3413bb8d5
webprog26 <webprog26@gmail.com>
2017-10-05 16:15:34 +0300

    Project created

---
+++ app/src/main/java/com/example/webprog26/dummymaster/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.webprog26.dummymaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.tv_text)
+    TextView mTvText;
+
+    @BindView(R.id.btn_click)
+    Button mBtnClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        getBtnClick().setOnClickListener(this);
+    }
+
+    public TextView getTvText() {
+        return mTvText;
+    }
+
+    public Button getBtnClick() {
+        return mBtnClick;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_click:
+
+
+
+                break;
+        }
+
+    }
+}
